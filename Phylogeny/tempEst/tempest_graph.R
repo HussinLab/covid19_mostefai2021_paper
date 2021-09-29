3112d3847412ec04820f0d84515dba5551a57d3c
Jean-Christophe Grenier <jcg@MacBook-Pro-de-Jean-Christophe.local>
2021-09-29 14:36:06 -0400

    Correction ggplot2 line

--- Phylogeny/tempEst/tempest_graph.R
+++ Phylogeny/tempEst/tempest_graph.R
@@ -7,5 +7,5 @@ df_tempest_merge=merge(df_tempest,df_dates,by.x="tip",by.y="name")
 df_tempest_merge$haplotype = factor(df_tempest_merge$haplotype,levels=haps)
 
 pdf("RootToTip.perHap.pdf")
-ggplot(df_tempest_merge,aes(x=as.Date(date),y=distance,color=as.factor(haplotype)))+geom_point(alpha=1)+scale_color_manual(values=colours_hap[as.factor(df_tempest_merge$haplotype)],name = "Haplotype")+theme_class
+ggplot(df_tempest_sub,aes(x=as.Date(date),y=distance,color=as.factor(haplotype)))+geom_point(alpha=1)+scale_color_manual(values=colours_hap[as.factor(df_tempest_sub$haplotype)],name = "Haplotype")+theme_classic()+theme(axis.text.x = element_text(angle = 45, hjust=1))
 dev.off()
